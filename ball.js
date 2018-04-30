8445b67ec9251e8ffe703af95d2a260ce54bf219
moryw <matt@skitskat.xyz>
2018-04-30 15:23:37 +0700

    Add switch statment for assigning answers to ball and log answer.

--- ball.js
+++ ball.js
@@ -9,3 +9,43 @@ userName ? console.log(`${userName} asked: ${userQuestion}.`) : console.log(`You
 let randomNumber = Math.floor(Math.random() * 8);
 
 let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+  default:
+    eightBall = 'Death is coming soon';
+    break;
+}
+
+userName ? console.log(`The answer to your question ${userName} is: ${eightBall}.`) : console.log(`The answer to your quetion is: ${eightBall}.`);
+
+
+
+
+
+
+
+//end
