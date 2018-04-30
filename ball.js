fc009079ca880ebcc0123d65e71721875944bee0
moryw <matt@skitskat.xyz>
2018-04-30 15:32:30 +0700

    Add an if/else statment to practice writing control flow differently

--- ball.js
+++ ball.js
@@ -9,7 +9,7 @@ userName ? console.log(`${userName} asked: ${userQuestion}.`) : console.log(`You
 let randomNumber = Math.floor(Math.random() * 8);
 
 let eightBall = '';
-
+//one way to write a control flow statment
 switch (randomNumber) {
   case 0:
     eightBall = 'It is certain';
@@ -38,6 +38,27 @@ switch (randomNumber) {
   default:
     eightBall = 'Death is coming soon';
     break;
+}
+
+//second way to write this control flow statment but using if/else instead of switch
+if (randomNumber === 0) {
+  eightBall = 'It is certain';
+} else if (randomNumber === 1) {
+  eightBall = 'It is decidedly so';
+} else if (randomNumber === 2) {
+  eightBall = 'Reply hazy try again';
+} else if (randomNumber === 3) {
+  eightBall = 'Cannot predict now';
+} else if (randomNumber === 4) {
+  eightBall = 'Do not count on it';
+} else if (randomNumber === 5) {
+  eightBall = 'My sources say no';
+} else if (randomNumber === 6) {
+  eightBall = 'Outlook not so good';
+} else if (randomNumber === 7) {
+  eightBall = 'Signs point to yes';
+} else {
+  eightBall = 'Death is coming soon';
 }
 
 userName ? console.log(`The answer to your question ${userName} is: ${eightBall}.`) : console.log(`The answer to your quetion is: ${eightBall}.`);
