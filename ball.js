2ad11e82716c91bd4bee62acdbb5b834a6accf35
moryw <matt@skitskat.xyz>
2018-04-30 15:11:13 +0700

    Add random number between 0 and 8 and the eightball var

--- ball.js
+++ ball.js
@@ -5,3 +5,7 @@ userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 let userQuestion = 'How old am I?';
 
 userName ? console.log(`${userName} asked: ${userQuestion}.`) : console.log(`You asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
