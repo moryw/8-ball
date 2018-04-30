2b7fabf1ec9602a1df0eee2edd248bc72b68fb0c
moryw <matt@skitskat.xyz>
2018-04-30 15:08:17 +0700

    Start 8 ball project, add user name and question and log results.

---
+++ ball.js
@@ -0,0 +1,7 @@
+let userName = 'Matt';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+let userQuestion = 'How old am I?';
+
+userName ? console.log(`${userName} asked: ${userQuestion}.`) : console.log(`You asked: ${userQuestion}`);
